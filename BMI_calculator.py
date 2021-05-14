8f794d49f6e55c73da281a20179a18ecdb134478
Linh Quach <Linh.M.Quach1@gmail.com>
2021-05-13 23:05:47 -0500

    This code prompts user for height and weight input. Then BMI is calculated after user inputs height and weight.

---
+++ BMI_calculator.py
@@ -0,0 +1,23 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ") 
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+BMI= float(weight) / (float(height)** 2)
+
+print (int(BMI))
+
+
+
+# Another Method for this Exercise
+print( "\nRunning this a second time please enter information below: \n")
+
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+
+
+#Write your code below this line 👇
+BMI= float(weight) / (float(height)** 2)
+
+print (int(BMI))
