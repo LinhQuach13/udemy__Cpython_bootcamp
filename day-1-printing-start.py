9fa73bb731545480d28afac7636cbf850eaf6c9d
Linh Quach <Linh.M.Quach1@gmail.com>
2021-11-30 12:43:53 -0600

    updated

---
+++ day-1-printing-start.py
@@ -0,0 +1,18 @@
+# Write your code below this line 👇
+print("Hello world!")
+
+
+#hello world with new lines
+print("Hello World!\n Hello World!\n Hello World!")
+
+
+#concat strings
+print("Hello" + "Angela")
+
+
+#concat with space
+print("Hello" + " " + "Angela")
+
+
+#input () function
+input("What is your name?")
