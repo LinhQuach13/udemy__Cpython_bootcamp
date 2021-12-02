4fcc31f1af0d227f5e1c330a0363db1713e65b0a
Linh Quach <Linh.M.Quach1@gmail.com>
2021-12-02 14:45:16 -0600

    adding

---
+++ day-1-variable-start.py
@@ -0,0 +1,16 @@
+#setting name variable to Jack
+name = "Jack"
+#Printing name variable
+print(name)
+
+#setting name variable to Jack
+name = "Angela"
+#Printing name variable
+print(name)
+
+#input name and set to name variable
+name= input("What is your name? ")
+#get length of input and set to length variable name
+length = len(name)
+#Print variable named length
+print(length)
