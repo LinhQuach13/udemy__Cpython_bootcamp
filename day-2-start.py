ec9a71e721da4778aeceb8897dc91daddf602ea9
Linh Quach <Linh.M.Quach1@gmail.com>
2021-12-03 18:24:46 -0600

    adding

---
+++ day-2-start.py
@@ -0,0 +1,23 @@
+#Data Types
+
+#Strings
+
+#print the first character of the string
+#(Known as Subscripting)
+print("Hello"[0])
+
+#print last character of the string
+print("Hello"[4])
+
+#Integer (#'s without any decimal places)
+print(123 + 345)
+
+#Can add _ to act as commas visually and _ will be ignored when it's printed
+print(123_456_678)
+
+#float (#'s with decimal points)
+
+print(3.14159)
+
+#Boolean
+# True Or False
