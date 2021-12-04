75efc7116c24c8463b77ee50f70262e6c6e29507
Linh Quach <Linh.M.Quach1@gmail.com>
2021-12-03 18:25:04 -0600

    adding

---
+++ day-2-1-exercise.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+#can subscript strings to get hold of a particular value at a particular position and then convert it into an integer 
+# to be able perform math on them
+first_digit= int(two_digit_number[0])
+
+second_digit= int(two_digit_number[1])
+
+print(first_digit + second_digit)
